d5f413da9041647b7b8820cdf80b59b18f04eb8f
bnk44 <47454628+bnk44@users.noreply.github.com>
2022-12-02 04:47:32 +0300

    Add files via upload

---
+++ EnemyManager.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    public class EnemyManager : CharacterManager
+    {
+        EnemyLocomotionManager enemyLocomotionManager;
+        EnemyAnimatorManager enemyAnimationManager;
+
+        public bool isPreformingAction;
+
+        public EnemyAttackAction[] enemyAttacks;
+        public EnemyAttackAction currentAttack;
+
+        [Header("A.I Settings")]
+        public float detectionRadius = 20;
+        //The higher, and lower, respectively these angles are, the greater detection FIELD OF VIEW (basically like eye sight)
+        public float maximumDetectionAngle = 50;
+        public float minimumDetectionAngle = -50;
+
+        public float currentRecoveryTime = 0;
+
+        private void Awake()
+        {
+            enemyLocomotionManager = GetComponent<EnemyLocomotionManager>();
+            enemyAnimationManager = GetComponentInChildren<EnemyAnimatorManager>();
+        }
+
+        private void Update()
+        {
+            HandleRecoveryTimer();
+        }
+
+        private void FixedUpdate()
+        {
+            HandleCurrentAction();
+        }
+
+        private void HandleCurrentAction()
+        {
+            if (enemyLocomotionManager.currentTarget != null)
+            {
+                enemyLocomotionManager.distanceFromTarget =
+                    Vector3.Distance(enemyLocomotionManager.currentTarget.transform.position, transform.position);
+            }
+            if (enemyLocomotionManager.currentTarget == null)
+            {
+                enemyLocomotionManager.HandleDetection();
+            }
+            else if (enemyLocomotionManager.distanceFromTarget > enemyLocomotionManager.stoppingDistance)
+            {
+                enemyLocomotionManager.HandleMoveToTarget();
+            }
+            else if (enemyLocomotionManager.distanceFromTarget <= enemyLocomotionManager.stoppingDistance)
+            {
+                AttackTarget();
+            }
+        }
+
+        private void HandleRecoveryTimer()
+        {
+            if (currentRecoveryTime > 0)
+            {
+                currentRecoveryTime -= Time.deltaTime;
+            }
+
+            if (isPreformingAction)
+            {
+                if (currentRecoveryTime <= 0)
+                {
+                    isPreformingAction = false;
+                }
+            }
+        }
+
+        #region Attacks
+
+        private void AttackTarget()
+        {
+            if (isPreformingAction)
+                return;
+
+            if (currentAttack == null)
+            {
+                GetNewAttack();
+            }
+            else
+            {
+                isPreformingAction = true;
+                currentRecoveryTime = currentAttack.recoveryTime;
+                enemyAnimationManager.PlayTargetAnimation(currentAttack.actionAnimation, true);
+                currentAttack = null;
+            }
+        }
+        private void GetNewAttack()
+        {
+            Vector3 targetsDirection = enemyLocomotionManager.currentTarget.transform.position - transform.position;
+            float viewableAngle = Vector3.Angle(targetsDirection, transform.forward);
+            enemyLocomotionManager.distanceFromTarget = Vector3.Distance(enemyLocomotionManager.currentTarget.transform.position, transform.position);
+
+            int maxScore = 0;
+
+            for (int i = 0; i < enemyAttacks.Length; i++)
+            {
+                EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+                if (enemyLocomotionManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack
+                    && enemyLocomotionManager.distanceFromTarget >= enemyAttackAction.minimumDistanceNeededToAttack)
+                {
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle
+                        && viewableAngle >= enemyAttackAction.minimumAttackAngle)
+                    {
+                        maxScore += enemyAttackAction.attackScore;
+                    }
+                }
+            }
+
+            int randomValue = Random.Range(0, maxScore);
+            int temporaryScore = 0;
+
+            for (int i = 0; i < enemyAttacks.Length; i++)
+            {
+                EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+                if (enemyLocomotionManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack
+                    && enemyLocomotionManager.distanceFromTarget >= enemyAttackAction.minimumDistanceNeededToAttack)
+                {
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle
+                        && viewableAngle >= enemyAttackAction.minimumAttackAngle)
+                    {
+                        if (currentAttack != null)
+                            return;
+
+                        temporaryScore += enemyAttackAction.attackScore;
+
+                        if (temporaryScore > randomValue)
+                        {
+                            currentAttack = enemyAttackAction;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ PlayerManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+        CameraHandler cameraHandler;
+        PlayerLocomotion playerLocomotion;
+        
+    
+        
+
+
+
+        public bool isInteracting;
+
+        [Header("Player Flags")]
+        public bool isSprinting;
+        public bool isInAir;
+        public bool isGrounded;
+        public bool canDoCombo;
+
+        private void Awake()
+        {
+            cameraHandler = FindObjectOfType<CameraHandler>();
+        }
+
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+        }
+
+
+        void Update()
+        {
+            float delta = Time.deltaTime;
+            isInteracting = anim.GetBool("isInteracting");
+            canDoCombo = anim.GetBool("canDoCombo");
+
+            inputHandler.TickInput(delta);
+            playerLocomotion.HandleMovement(delta);
+            playerLocomotion.HandleRollingAndSprinting(delta);
+            playerLocomotion.HandleFalling(delta, playerLocomotion.moveDirection);
+            CheckForInteractableObject(); 
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+            inputHandler.rb_Input = false;
+            inputHandler.rt_Input = false;
+            inputHandler.d_Pad_Up = false;
+            inputHandler.d_Pad_Down = false;
+            inputHandler.d_Pad_Left = false;
+            inputHandler.d_Pad_Right = false;
+            inputHandler.x_Input = false;
+
+            if (isInAir)
+            {
+                playerLocomotion.inAirTimer = playerLocomotion.inAirTimer + Time.deltaTime;
+            }
+        }
+
+        public void CheckForInteractableObject()
+        {
+            RaycastHit hit;
+
+            if (Physics.SphereCast(transform.position, 0.3f, transform.forward, out hit, 1f))
+            {
+                if (hit.collider.tag == "Interactable")
+                {
+                    Interactable interactableObject = hit.collider.GetComponent<Interactable>();
+
+                    if (interactableObject != null)
+                    {
+                        string interactableText = interactableObject.interactbleText;
+                        //SET THE UI TEXT TO THE INTERACTABLE OBJECT'S TEXT
+                        //SET THE TEXT POP UP TO TRUE
+
+                        if (inputHandler.x_Input)
+                        {
+                            hit.collider.GetComponent<Interactable>().Interact(this);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
